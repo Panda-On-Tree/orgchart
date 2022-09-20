173c94fb7e207438a95e1c19a70b2c47a0223087
Aniket Khatri <aksonipat2000@gmail.com>
2022-09-20 16:19:04 +0530

    Update apiConfig.js

--- ess/src/api/apiConfig.js
+++ ess/src/api/apiConfig.js
@@ -1,5 +1,6 @@
 export const baseurl ={
    // base_url :"http://microtek.tech:8443/v1/api",
   //base_url: "http://192.168.1.7:8080/v1/api"  ,
-  base_url:"http://192.186.1.6:8443/v1/api"
+  //base_url:"https://103.25.174.74/v1/api"
+  base_url:"http://192.168.1.6:8443/v1/api"
   }
