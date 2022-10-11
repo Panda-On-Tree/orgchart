9d7a2a30f3e0a2f16159f3a53f9cf28d4ffc79d3
Aniket Khatri <aksonipat2000@gmail.com>
2022-10-11 12:27:47 +0530

    Update Login.jsx

--- ess/src/Pages/Login.jsx
+++ ess/src/Pages/Login.jsx
@@ -33,7 +33,7 @@ const LoginForm = () => {
         localStorage.setItem('fullname', response.data.name_of_the_employee)
         localStorage.setItem('email', response.data.email)
         localStorage.setItem('role', response.data.role)
-        navigate('/chart')
+        navigate('/home')
         window.location.reload()
       })
       .catch(function (err) {
